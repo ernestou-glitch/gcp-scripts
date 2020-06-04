840d4242357cd2e692d731bc3e823c5bb464ba20
ernestou-glitch <61810527+ernestou-glitch@users.noreply.github.com>
2020-06-04 09:10:51 -0500

    Create variable.tf

---
+++ terraform/variables_outputs_interpolation/variable.tf
@@ -0,0 +1,14 @@
+# Terraform works on folder level so it knows will search for important files such as provider.tf or variable.tf in the same folder where main.tf is located
+# https://www.terraform.io/docs/configuration/variables.html
+variable "image" { default = "ubuntu-os-cloud/ubuntu-1604-lts"}
+variable "machine_type" { 
+    type = "map"
+    default = {
+    "dev" = "n1-standard-1"
+    "prod" = "production_box_wont_work"
+    } #variable type MAP in your main.tf file you need to specify to which value you are going to map it
+    #var.machine_type["dev"]
+    #var.machine_type["prod"]
+}
+variable "zone" { default = "us-central1-a"}
+variable "name_count" {default = ["server1","server2","server3"]} #this is a variable type LIST
