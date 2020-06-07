ab7d91e7ad8fe832246aeffb88877c33ff95d23f
ernestou-glitch <61810527+ernestou-glitch@users.noreply.github.com>
2020-06-07 09:15:05 -0500

    Create firewall.tf

---
+++ terraform/advanced_instance/firewall.tf
@@ -0,0 +1,22 @@
+#https://www.terraform.io/docs/providers/google/r/compute_firewall.html
+resource "google_compute_firewall" "allow_http_fw" {
+    name = "allow-http"
+    network = "default"
+
+    allow {
+        protocol = "tcp"
+        ports = ["80","8080"]
+    }
+    target_tags = ["allow-http-tag"]
+}
+
+resource "google_compute_firewall" "allow_https_fw" {
+    name = "allow-https"
+    network = "default"
+
+    allow {
+        protocol = "tcp"
+        ports = ["443"]
+    }
+    target_tags = ["allow-https-tag"]
+}
