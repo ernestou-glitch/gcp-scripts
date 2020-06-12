2416394571a50362fe686c712109d368d77608d1
ernestou-glitch <61810527+ernestou-glitch@users.noreply.github.com>
2020-06-12 18:59:23 -0500

    Update variables.tf

--- terraform/biguery/variables.tf
+++ terraform/biguery/variables.tf
@@ -1,6 +1,6 @@
 #Variables for provider.tf file
-variable "path" {default = "/home/ernesto_urquijo/FEDEX/credentials"}
-variable "project_id" {default = "ernestoandres2"}
+variable "path" {default = "/home/.........."}
+variable "project_id" {default = "xxxxxxxxxxxx"}
 variable "gcp_region" {default = "us-central1"}
 
 
