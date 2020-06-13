ecd34a9e1170577dbb96f3d191df95edcb4cc15c
ernestou-glitch <61810527+ernestou-glitch@users.noreply.github.com>
2020-06-12 19:00:05 -0500

    Update variable.tf

--- terraform/modules/variable.tf
+++ terraform/modules/variable.tf
@@ -1,4 +1,4 @@
 #Variables for provider.tf file
-variable "path" {default = "/home/ernesto_urquijo/FEDEX/credentials"}
-variable "project_id" {default = "ernestoandres2"}
+variable "path" {default = "/home/........"}
+variable "project_id" {default = "xxxxxxxx"}
 variable "gcp_region" {default = "us-central1"}
