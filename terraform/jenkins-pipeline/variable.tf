8e33f23fa956307c24a75f3f815088926100c9a7
ernestou-glitch <61810527+ernestou-glitch@users.noreply.github.com>
2020-06-13 18:22:33 -0500

    Add files via upload

---
+++ terraform/jenkins-pipeline/variable.tf
@@ -0,0 +1,10 @@
+# Terraform works on folder level so it knows will search for important files such as provider.tf or variable.tf in the same folder where main.tf is located
+# https://www.terraform.io/docs/configuration/variables.html
+variable "path" {default = "/home/ernesto_urquijo/FEDEX/credentials"}
+variable "project_id" {default = "ernestoandres2"}
+variable "gcp_region" {default = "us-central1"}
+
+variable "image" { default = "ubuntu-os-cloud/ubuntu-1804-lts"}
+variable "machine_type" { default = "n1-standard-1"}
+variable "environment" {default = "prod"}
+variable "zone" { default = "us-central1-a"}
