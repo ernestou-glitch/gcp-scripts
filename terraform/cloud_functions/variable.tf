f47bdc4e4df28240340b76f92b42116364bf75a2
ernestou-glitch <61810527+ernestou-glitch@users.noreply.github.com>
2020-06-13 17:53:38 -0500

    Update variable.tf

--- terraform/cloud_functions/variable.tf
+++ terraform/cloud_functions/variable.tf
@@ -1,16 +1,12 @@
 # Terraform works on folder level so it knows will search for important files such as provider.tf or variable.tf in the same folder where main.tf is located
 # https://www.terraform.io/docs/configuration/variables.html
 
-variable "path" {default = "/home/ernesto_urquijo/credentials"}
-variable "project_id" {default = "ernestoandres2"}
+variable "path" {default = "/home/........."}
+variable "project_id" {default = "xxxxxxxx"}
 variable "region" {default = "us-central1"}
 
-variable "python_code" {default = "/home/ernesto_urquijo/functions/hello_world/"}
+variable "python_code" {default = "/home/......./hello_world/"}
 variable "function_name" {default = "terraform-function"}
 variable "languaje" {default = "python37"}
 variable "memory_size" {default = "128"}
-variable "folder_path" { default = "/home/ernesto_urquijo/functions/hello_world"}
-
-
-
-"${var.path}/secret.json"
+variable "folder_path" { default = "/home/......./hello_world"}
