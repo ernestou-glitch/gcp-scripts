7ba7ebfebe1cf64a810898810bdc7173b760eacc
ernestou-glitch <61810527+ernestou-glitch@users.noreply.github.com>
2020-06-13 18:23:01 -0500

    Update variable.tf

--- terraform/jenkins-pipeline/variable.tf
+++ terraform/jenkins-pipeline/variable.tf
@@ -1,7 +1,7 @@
 # Terraform works on folder level so it knows will search for important files such as provider.tf or variable.tf in the same folder where main.tf is located
 # https://www.terraform.io/docs/configuration/variables.html
-variable "path" {default = "/home/ernesto_urquijo/FEDEX/credentials"}
-variable "project_id" {default = "ernestoandres2"}
+variable "path" {default = "/home/......"}
+variable "project_id" {default = "xxxxxx"}
 variable "gcp_region" {default = "us-central1"}
 
 variable "image" { default = "ubuntu-os-cloud/ubuntu-1804-lts"}
