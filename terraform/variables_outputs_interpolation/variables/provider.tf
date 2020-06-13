bd4891f25a213d06f9727720a11cad254c44a547
ernestou-glitch <61810527+ernestou-glitch@users.noreply.github.com>
2020-06-12 19:05:13 -0500

    Update provider.tf

--- terraform/variables_outputs_interpolation/variables/provider.tf
+++ terraform/variables_outputs_interpolation/variables/provider.tf
@@ -1,10 +1,10 @@
 #the information from this file will be used to authenticate to the project
 #https://www.terraform.io/docs/providers/google/index.html
 
-variable "path" {default = "/home/ernesto_urquijo/credentials"}
+variable "path" {default = "/home/......."}
 
 provider "google" {
-    project     = "ernestoandres2"
+    project     = "xxxxx"
     region      = "us-central"
     credentials = "${file("${var.path}/secret.json")}" #Terraform will search for file secret.json in the value of path variable
 }
