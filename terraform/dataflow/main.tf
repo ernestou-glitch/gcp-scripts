c0c15a62b3c1ded8ba6d10bfd9f8747c22d3e014
Emmanuel <e.beltranfs@gmail.com>
2020-06-16 12:19:21 -0500

    Dataflow changes

--- terraform/dataflow/main.tf
+++ terraform/dataflow/main.tf
@@ -3,13 +3,26 @@
 
 resource "google_dataflow_job" "big_data_job" {
   name              = var.name
-  template_gcs_path = var.temp_path
-  temp_gcs_location = var.out_path
+  template_gcs_path = var.template_path
+  temp_gcs_location = var.temp_dir
+  region = var.region
   zone = var.zone
+
+  #Optional parameters
+
+  # service_account_email = var.service_account
+  # network = var.network
+  # subnetwork = var.subnetwork
+  # machine_type = var.machine_type
+  # max_workers = var.max_workers
+  # additional_experiments = var.additional_experiments
+
+  #Dataflow Template parameters, example for WordCount Template
+  #https://cloud.google.com/dataflow/docs/guides/templates/provided-templates#wordcount
+
   parameters = {
-      status = "new"
-      iteration = "fisrt"
-      environment = "test"
+      inputFile = var.input
+      output = var.output
   }
   
 }
--- terraform/dataflow/variable.tf
+++ terraform/dataflow/variable.tf
@@ -1,11 +1,26 @@
 # Terraform works on folder level so it knows will search for important files such as provider.tf or variable.tf in the same folder where main.tf is located
 # https://www.terraform.io/docs/configuration/variables.html
 
-variable "path" {default = "/home/........"}
-variable "project_id" {default = "xxxxxx"}
+variable "path" {default = "/home....."}
+variable "project_id" {default = "xxxxxxxxx"}
 variable "region" {default = "us-central1"}
 variable "zone" {default = "us-central1-a"}
 
 variable "name" { default = "first-job"}
-variable "temp_path" {default = "gs://dataflow-templates/latest/Word_Count"}
-variable "out_path" {default = "gs://data-test-tf"}
+variable "template_path" {default = "gs://dataflow-templates/latest/Word_Count"}
+variable "temp_dir" {default = "gs://bucket-name/tmp"}
+
+#Optional parameters
+# variable "service_account_email" {default = "service_account@email.com"} 
+# variable "network" {default = "default"} 
+# variable "subnetwork" {default = "regions/REGION/subnetworks/SUBNETWORK"} 
+# variable "machine_type" {default = n1-standard-1} 
+# variable "max_workers" {default = 1} 
+# variable "additional_experiments" {default = ["enable_stackdriver_agent_metrics"]} 
+
+#Dataflow Template parameters, example for WordCount Template
+#https://cloud.google.com/dataflow/docs/guides/templates/provided-templates#wordcount
+
+variable "input" {default = "gs://bucket-name/inputfile"}
+variable "output" {default = "gs://bucket-name/outputfile"}
+
