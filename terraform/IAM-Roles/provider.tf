2d427471b56b748816322c4329369e0b3f705fd9
Ernesto Andres Urquijo Gonzalez <61810527+ernestou-glitch@users.noreply.github.com>
2020-12-03 14:24:21 -0600

    Update provider.tf

--- terraform/IAM-Roles/provider.tf
+++ terraform/IAM-Roles/provider.tf
@@ -2,7 +2,6 @@
 #https://www.terraform.io/docs/providers/google/index.html
 
 provider "google" {
-    project     = "${var.project_id}"
-    region      = "${var.gcp_region}"
-    credentials = "${file("${var.path}/secret.json")}" #Terraform will search for file secret.json in the value of path variable
+    project     = var.project_id
+    region      = var.gcp_region
 }
